08bcd3a872b0ce223b011955bb7c66197b93f782
Sanjana Krishnan <skmk0503@gmail.com>
2026-02-01 18:01:18 +0530

    update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,21 @@ import CSVUploads from "./components/CSVUploads";
 import Dashboards from "./components/Dashboards";
 import Login from "./components/Login";
 import History from "./components/History";
-
+import "./App.css";
 function App() {
   const [page, setPage] = useState("login");
   const [summary, setSummary] = useState(null);
 
   return (
     <div className="App">
+      
+      <h1 className="title" >Chemical Equipment Parameter Visualizer</h1>
+      <div className="navigate">
+        <button onClick={() => setPage("login")}>Login</button>
+        <button onClick={() => setPage("upload")}>Upload CSV</button>
+        <button onClick={() => setPage("history")}>History</button>
+      </div>
 
-      <h1>Chemical Equipment Parameter Visualizer</h1>
-
-      {/* Navigation */}
-      <button onClick={() => setPage("login")}>Login</button>
-      <button onClick={() => setPage("upload")}>Upload CSV</button>
-      <button onClick={() => setPage("history")}>History</button>
-
-      {/* Pages */}
       {page === "login" && <Login />}
 
       {page === "upload" && (
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,41 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-function Login() {
+function Login({ onLoginSuccess }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://127.0.0.1:8000/api/token/", {
+      const response = await axios.post("http://127.0.0.1:8000/api/token/", {
         username,
         password,
       });
 
-      localStorage.setItem("token", res.data.access);
-      alert("Login successful");
-    } catch (e) {
-      alert("Retry invalid credentials");
+      localStorage.setItem("token", response.data.access);
+      alert("Login successful!");
+      onLoginSuccess();
+    } 
+    catch (err) {
+      alert("Invalid credentials");
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "80vh" }}>
+      <div className="card shadow p-4" style={{ width: "350px" }}>
+        <h3 className="text-center mb-3">Login</h3>
 
-      <input
-        type="text"
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
-      />
+        <input type="text" className="form-control mb-3" placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
 
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <input type="password" className="form-control mb-3" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
-      <button onClick={handleLogin}>Login</button>
+        <button className="btn btn-primary w-100" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
     </div>
   );
 }
